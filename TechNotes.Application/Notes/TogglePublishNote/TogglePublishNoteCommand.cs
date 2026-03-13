5ef4f56911a92f30079b4880fd5ec8c5696eb718
Cristian Sifuentes <sifuentescovarrubiasitsl@gmail.com>
2026-03-13 14:58:04 -0600

    Adding code for User Management With Interactivity Using Blazo rServer

---
+++ TechNotes.Application/Notes/TogglePublishNote/TogglePublishNoteCommand.cs
@@ -0,0 +1,6 @@
+namespace TechNotes.Application.Notes.TogglePublishNote;
+
+public class TogglePublishNoteCommand : ICommand<NoteResponse>
+{
+  public int NoteId { get; set; }
+}
---
+++ TechNotes.Application/Users/RemoveRoleFromUser/RemoveRoleFromUserCommand.cs
@@ -0,0 +1,7 @@
+namespace TechNotes.Application.Users.RemoveRoleFromUser;
+
+public class RemoveRoleFromUserCommand : ICommand
+{
+  public required string UserId { get; set; }
+  public required string RoleName { get; set; }
+}
